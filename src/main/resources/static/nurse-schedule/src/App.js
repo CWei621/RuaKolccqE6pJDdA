3882037fa0ace6a178f4076b815949c4562d9d1d
wayne <wayne0621@gapp.nthu.edu.tw>
2024-11-29 10:54:28 +0800

    feat: 前端功能初始化，加入後端CROS設定

--- src/main/resources/static/nurse-schedule/src/App.js
+++ src/main/resources/static/nurse-schedule/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import api from './api';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        api.get('/sites')
+            .then(response => console.log(response.data))
+            .catch(error => console.error('Error fetching sites:', error));
+    }, []);
+
+    return (
+        <div>
+            <h1>Nurse Schedule Management</h1>
+        </div>
+    );
 }
 
 export default App;
